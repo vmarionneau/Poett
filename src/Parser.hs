d3bd34a7289085c0e295f0539336fc8578709cd3
Virgil Marionneau <virgil.marionneau@ens-rennes.fr>
2025-11-15 17:35:01 +0100

    Modified lexer

--- src/Parser.hs
+++ src/Parser.hs
@@ -6,7 +6,7 @@ import Control.Applicative
 import Control.Monad (foldM, void)
 import Data.Char (ord)
 
-newtype Parser a b = Parser { runParser :: [a] → Maybe (b, [a]) }
+newtype Parser a b = Parser { runParser :: a → Maybe (b, a) }
 
 instance Functor (Parser a) where
   fmap f m = Parser (fmap (\ (b, as) → (f b, as)) . runParser m)
@@ -32,11 +32,18 @@ ma << mb =
     mb
     pure a
 
+data Stream a = Stream { streamData :: [a], streamRow :: Int, streamCol :: Int }
+  deriving (Eq, Show)
+
 parserFail :: Parser a b
 parserFail = Parser (const Nothing)
 
-getStream :: Parser a [a]
+getStream :: Parser a a
 getStream = Parser (\ s → Just (s,s))
+
+setStream :: a → Parser a ()
+setStream s = Parser (\ _ → Just ((),s))
+
 
 parseMaybe :: Parser a b → Parser a (Maybe b)
 parseMaybe p = Parser (\ s →
@@ -51,26 +58,77 @@ data Token
   | Lambda
   | TokPi
   | Let
+  | DefEq
   | In
   | LParen
   | RParen
+  | LBracket
+  | RBracket
   | Identifier String
   | Number Int
   | Comma
   | Colon
-  | LBracket
-  | RBracket
   | Bar
   deriving (Eq, Show)
 
-char :: Char → Parser Char Char
-char c = Parser aux
-  where
-    aux [] = Nothing
-    aux (k:ks) =
-      if c == k
-      then pure (c, ks)
-      else Nothing
+nextCol :: Parser (Stream a) ()
+nextCol =
+  do
+    str ← getStream
+    setStream $ str { streamCol = 1 + streamCol str }
+
+nextRow :: Parser (Stream a) ()
+nextRow =
+   do
+    str ← getStream
+    setStream $ str { streamCol = 0, streamRow = 1 + streamRow str }
+
+getStreamData :: Parser (Stream a) [a]
+getStreamData =
+  do
+    str ← getStream
+    pure $ streamData str
+
+setStreamData :: [a] → Parser (Stream a) ()
+setStreamData cs =
+  do
+    str ← getStream
+    setStream $ str { streamData = cs }
+
+next :: Parser (Stream Char) Char
+next =
+  do
+    cs ← getStreamData
+    case cs of
+      [] → parserFail
+      (c:cs) → (if c == '\n' then nextRow else nextCol) >> setStreamData cs >> pure c
+
+char :: Char → Parser (Stream Char) Char
+char c =
+  do
+    k ← next
+    if k == c
+      then pure c
+      else parserFail
+
+getCol :: Parser (Stream a) Int
+getCol =
+  do
+    str ← getStream
+    pure $ streamCol str
+
+getRow :: Parser (Stream a) Int
+getRow =
+  do
+    str ← getStream
+    pure $ streamRow str
+
+getPos :: Parser (Stream a) (Int, Int)
+getPos =
+  do
+    col ← getCol
+    row ← getRow
+    pure (row, col)
 
 peeking :: Parser a b → Parser a b
 peeking p =
@@ -90,16 +148,16 @@ notFollowed p f =
       Just _ → parserFail
       Nothing → pure res
 
-string :: String → Parser Char String
+string :: String → Parser (Stream Char) String
 string s = foldM (const $ void . char) () s >> pure s
 
 oneOf :: [Parser a b] → Parser a b
 oneOf = foldl ( <|> ) parserFail
 
-alpha :: Parser Char Char
+alpha :: Parser (Stream Char) Char
 alpha = oneOf $ char <$> "_abcdefghijklmnopqrtsuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-num :: Parser Char Char
+num :: Parser (Stream Char) Char
 num = oneOf $ char <$> "0123456789"
 
 readDigit :: Char → Parser a Int
@@ -108,78 +166,75 @@ readDigit c =
   then pure (ord c - ord '0')
   else parserFail
 
-digit :: Parser Char Int
+digit :: Parser (Stream Char) Int
 digit =
   do
     c ← num
     readDigit c
 
-alphaNum :: Parser Char Char
+alphaNum :: Parser (Stream Char) Char
 alphaNum = alpha <|> num
 
-identifier :: Parser Char Token
+identifier :: Parser (Stream Char) Token
 identifier =
   do
     c ← alpha
     rest ← many alphaNum
     pure $ Identifier $ c:rest
 
-number :: Parser Char Token
+number :: Parser (Stream Char) Token
 number =
   do
     d0 ← digit
     digits ← many digit
     pure $ Number $ foldl (\ i d → i * 10 + d) d0 digits
                     
-comma :: Parser Char Token
+comma :: Parser (Stream Char) Token
 comma = char ',' >> pure Comma
 
-colon :: Parser Char Token
+colon :: Parser (Stream Char) Token
 colon = char ':' >> pure Colon
 
-lbracket :: Parser Char Token
+lbracket :: Parser (Stream Char) Token
 lbracket = char '[' >> pure LBracket
 
-rbracket :: Parser Char Token
+rbracket :: Parser (Stream Char) Token
 rbracket = char ']' >> pure RBracket
 
-bar :: Parser Char Token
+bar :: Parser (Stream Char) Token
 bar = char '|' >> pure Bar
 
-lambda :: Parser Char Token
+lambda :: Parser (Stream Char) Token
 lambda = notFollowed (string "λ" <|> string "fun") alphaNum >> pure Lambda
 
-piTok :: Parser Char Token
+piTok :: Parser (Stream Char) Token
 piTok = notFollowed (string "Π" <|> string "Pi") alphaNum >> pure TokPi
 
-letTok :: Parser Char Token
+letTok :: Parser (Stream Char) Token
 letTok = notFollowed (string "let") alphaNum >> pure Let
 
-inTok :: Parser Char Token
+defEq :: Parser (Stream Char) Token
+defEq = notFollowed (string ":=") alphaNum >> pure DefEq
+
+inTok :: Parser (Stream Char) Token
 inTok = notFollowed (string "in") alphaNum >> pure In
 
-definition :: Parser Char Token
+definition :: Parser (Stream Char) Token
 definition = notFollowed (string "def") alphaNum >> pure Definition
 
-inductive :: Parser Char Token
+inductive :: Parser (Stream Char) Token
 inductive = notFollowed (string "ind") alphaNum >> pure Inductive
 
-lparen :: Parser Char Token
+lparen :: Parser (Stream Char) Token
 lparen = char '(' >> pure LParen
 
-rparen :: Parser Char Token
+rparen :: Parser (Stream Char) Token
 rparen = char ')' >> pure LParen
 
-whitespace :: Parser Char String
-whitespace = many $ oneOf $ char <$> " \t\r"
+whitespace :: Parser (Stream Char) String
+whitespace = many $ oneOf $ char <$> " \t\r\n"
 
-indent :: Parser Char Int
-indent =
-  do
-    pre ← many $ char ' '
-    pure $ length pre
-
-token :: Parser Char Token
+token :: Parser (Stream Char) Token
 token = oneOf
         [ lambda
         , piTok
@@ -191,39 +246,19 @@ token = oneOf
         , rbracket
         , number
         , comma
-        , colon
         , bar
         , letTok
+        , defEq
+        , colon
         , inTok
         , identifier]
 
-offset :: Parser a b → Parser a (b, Int)
-offset p =
+tokenPos :: Parser (Stream Char) (Token, (Int, Int))
+tokenPos =
   do
-    len0 ← length <$> getStream
-    res ← p
-    len1 ← length <$> getStream
-    pure (res, len0 - len1)
+    pos ← getPos
+    tok ← token
+    pure (tok, pos)
 
-lexerLine :: Parser Char ([(Token, Int)])
-lexerLine = rep 0 << whitespace
-  where
-    rep i =
-      do
-        w ← length <$> whitespace
-        res ← parseMaybe (offset token)
-        case res of
-          Nothing → pure []
-          Just (tok, j) →
-            do
-              { rest ← rep (i + w + j)
-              ; pure $ (tok, i + w):rest
-              }
-  
-lexer :: Parser Char [[(Token, Int)]]
-lexer =
-  do
-    lns ← many (lexerLine << char '\n')
-    final ← lexerLine
-    pure (lns ++ [final])
-    
+lexer :: Parser (Stream Char) [(Token, (Int, Int))]
+lexer = many (tokenPos << whitespace)
